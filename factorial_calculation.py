c948b9d15b19889b4d363813149278e385f12e35
SasukeAlHashemi <149956167+SasukeAlHashemi@users.noreply.github.com>
2025-02-09 14:50:34 +0530

    Delete factorial_calculation.py

--- factorial_calculation.py
+++
@@ -1,7 +0,0 @@
-number = int(input("Enter number: "))
-n, result = number, 1
-while n > 0:
-    result = result * n
-    n = n - 1
-factorial = result
-print("Factorial of", number, "is", factorial)
